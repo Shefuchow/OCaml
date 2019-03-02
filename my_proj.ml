b3a03521b2f438286224ec1965c14d417cf57b06
Mohammed Sefath Chowdhury <mschowdh@buffalo.edu>
2019-03-02 00:19:19 -0500

    more recursive function practice

--- my_proj.ml
+++ my_proj.ml
@@ -43,6 +43,7 @@ let square x = x *. x ;;
  * x @ y;; appending lists
  * List.hd list;; Head of list
  * List.tl list;; tail is list
+ * polymorphic (primitive, strings, objects, even functions)
  *)
 let l = ["is"; "a"; "tale"; "told"];;
 (* string list = ["is"; "a"; "tale"; "told"] *)
@@ -73,7 +74,35 @@ let rec triple_list_elements = function
   | [] -> []
   | head::tail -> head :: head :: head :: duplicate_list_elements tail
 
-let rec reverse_list list6 reverseList = 
+let rec reverse_list list6 reverseList =
   match list6 with
   | [] -> reverseList
   | head :: tail -> reverse_list tail reverseList@[head]
+
+let rec element_at myList index =
+  match myList with
+  | [] -> raise (Failure "empty list")
+  | h::t ->
+    if index = 0 then h
+    else element_at t(index-1)
+
+let rec element_at2 myList index =
+  match myList with
+  |[]                   -> raise (Failure "empty list")
+  | h::_ when index=0   -> h
+  | _::t                -> element_at2 t (index-1)
+
+let rec range a b result =
+  if a=b then
+    result@[b]
+  else
+    range (a+1) b (result@[a])
+
+let range a b =
+  let rec aux a b =
+    if a > b then
+      []
+    else
+      a :: aux (a+1) b
+    in
+    aux a b
