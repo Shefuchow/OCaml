95caf3c2c49058418f4532ef7240181960bdcff6
Mohammed Sefath Chowdhury <mschowdh@buffalo.edu>
2019-03-01 20:26:23 -0500

    two ways to compile using command lne

--- my_proj.ml
+++ my_proj.ml
@@ -6,3 +6,12 @@ print_endline "Hello, World!";;
  * multi-line
  * comment.
 *)
+
+(* ----- How to compile projects? ----- *)
+(* After we made our program in our fav IDE,
+ * run `ocamlbuild <file_name>.native`
+ * run `./<file_name>.native`
+ * bytecode compiler
+ * run `$ ocamlc -o <file_name> <file_name>.ml
+ * run `$ ./<file_name>`
+ *)
